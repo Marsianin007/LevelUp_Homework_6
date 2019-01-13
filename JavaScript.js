6f7f28a8f1727934e8c24b02c30b058c2b840450
Marsianin007 <chernoborodiuk20@gmail.com>
2019-01-13 20:13:37 +0200

    JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+<noscript>Check settings</noscript>
+var name=prompt("Please write your name");
+if(name.length>6){
+    document.write("Привет " +name);
+}
+
+else{
+    document.write("Добрый день " +name);
+}
+
